18e25bc6fbc03dfd480c7b79dab43bc6aaa076e4
lmk375 <lmk375@drexel.edu>
2025-10-27 19:56:20 +0100

    Start on execute functions

--- src/execute.c
+++ src/execute.c
@@ -2,50 +2,68 @@
 
 
 int handleRType(decoded_fields instr) {
-    uint32_t rs1_val = regs[instr.r.rs1];
+    uint32_t rs1_val = regs[instr.r.rs1]; 
     uint32_t rs2_val = regs[instr.r.rs2];
     uint32_t result = 0;
 
     switch (instr.r.funct3) {
-        case F3_ADD_SUB:
-            result = (instr.r.funct7 == F7_SUB)
+        case F3_000: // ADD or SUB
+            // ADD: funct7 = 0x00
+            // SUB: funct7 = 0x20
+            result = (instr.r.funct7 == F7_0100000)
                 ? rs1_val - rs2_val
-                : rs1_val + rs2_val;
+                : rs1_val + rs2_val; 
             break;
-        case F3_SLL:
+    
+        case F3_001: // SLL
+            // Shift logical left
             result = rs1_val << (rs2_val & 0x1F);
             break;
-        case F3_SLT:
-            result = ((int32_t) rs1_val < (int32_t)rs2_val ) ? 1 : 0;
+    
+        case F3_010: // SLT
+            // SLT: set less than (signed)
+            result = ((int32_t) rs1_val < (int32_t) rs2_val) ? 1 : 0;
             break;
-        case F3_SLTU:
+    
+        case F3_011: // SLTU
+            // SLTU: set less than (unsigned)
             result = (rs1_val < rs2_val) ? 1 : 0;
             break;
-        case F3_XOR:
+    
+        case F3_100: // XOR
             result = rs1_val ^ rs2_val;
             break;
-        case F3_SRL_SRA:
-            result = (instr.r.funct7 == F7_SRA)
-                ? ((int32_t) rs1_val >> (rs2_val & 0x1F))
-                : (rs1_val >> (rs2_val & 0x1F));
+    
+        case F3_101: // SRL or SRA
+            // SRL: funct7 = 0x00
+            // SRA: funct7 = 0x20
+            result = (instr.r.funct7 == F7_0100000)
+                ? ((int32_t) rs1_val >> (rs2_val & 0x1F))   // SRA: shift right arithmetic (sign preserved)
+                : (rs1_val >> (rs2_val & 0x1F));    // SRL: shift right logical
             break;
-        case F3_OR:
+    
+        case F3_110: // OR
             result = rs1_val | rs2_val;
             break;
-        case F3_AND:
+    
+        case F3_111: // AND
             result = rs1_val & rs2_val;
             break;
-        default:
+    
+        default: // Unknown funct3
             return -1;
     }
-
+    
+    // Prevents destination register from updating if its the x0 (ZERO) register
     if (instr.r.rd != ZERO) {
         regs[instr.r.rd] = result;
     }
 
     return 0;
 }
-
+int handleIType(decoded_fields instr, uint8_t *memory){return 0;}
+int handleSType(decoded_fields instr, uint8_t *memory){return 0;}
+int handleUType(decoded_fields instr){return 0;}
 int handleBType(decoded_fields instr) {return 0;}
 int handleJType(decoded_fields instr) {return 0;}
 
