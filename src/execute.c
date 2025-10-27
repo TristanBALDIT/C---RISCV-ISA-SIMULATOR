29918ad04d2154906dd970f51df5ac8be8fb0520
lmk375 <lmk375@drexel.edu>
2025-10-27 20:33:41 +0100

    Handle U and B types

--- src/execute.c
+++ src/execute.c
@@ -1,9 +1,11 @@
 #include "../include/execute.h"
+#include <cstdint>
+#include <stdint.h>
 
 
 int handleRType(decoded_fields instr) {
-    uint32_t rs1_val = regs[instr.r.rs1]; // Soucre register
-    uint32_t rs2_val = regs[instr.r.rs2]; // Source register
+    uint32_t rs1 = regs[instr.r.rs1]; // Soucre register
+    uint32_t rs2 = regs[instr.r.rs2]; // Source register
     uint32_t result = 0; // The value to place in the destination register
 
     switch (instr.r.funct3) {
@@ -11,43 +13,43 @@ int handleRType(decoded_fields instr) {
             // ADD: funct7 = 0x00
             // SUB: funct7 = 0x20
             result = (instr.r.funct7 == F7_0100000)
-                ? rs1_val - rs2_val
-                : rs1_val + rs2_val; 
+                ? rs1 - rs2
+                : rs1 + rs2; 
             break;
     
         case F3_001: // SLL
             // Shift logical left
-            result = rs1_val << (rs2_val & 0x1F);
+            result = rs1 << (rs2 & 0x1F);
             break;
     
         case F3_010: // SLT
             // SLT: set less than (signed)
-            result = ((int32_t) rs1_val < (int32_t) rs2_val) ? 1 : 0;
+            result = ((int32_t) rs1 < (int32_t) rs2) ? 1 : 0;
             break;
     
         case F3_011: // SLTU
             // SLTU: set less than (unsigned)
-            result = (rs1_val < rs2_val) ? 1 : 0;
+            result = (rs1 < rs2) ? 1 : 0;
             break;
     
         case F3_100: // XOR
-            result = rs1_val ^ rs2_val;
+            result = rs1 ^ rs2;
             break;
     
         case F3_101: // SRL or SRA
             // SRL: funct7 = 0x00
             // SRA: funct7 = 0x20
             result = (instr.r.funct7 == F7_0100000)
-                ? ((int32_t) rs1_val >> (rs2_val & 0x1F))   // SRA: shift right arithmetic (sign preserved)
-                : (rs1_val >> (rs2_val & 0x1F));    // SRL: shift right logical
+                ? ((int32_t) rs1 >> (rs2 & 0x1F))   // SRA: shift right arithmetic (sign preserved)
+                : (rs1 >> (rs2 & 0x1F));    // SRL: shift right logical
             break;
     
         case F3_110: // OR
-            result = rs1_val | rs2_val;
+            result = rs1 | rs2;
             break;
     
         case F3_111: // AND
-            result = rs1_val & rs2_val;
+            result = rs1 & rs2;
             break;
     
         default: // Unknown funct3
@@ -68,9 +70,67 @@ int handleSType(decoded_fields instr, uint8_t *memory){
     return 0;
 }
 int handleUType(decoded_fields instr){
+    switch (instr.opcode) {
+        case LUI: // Load Upper Immediate (loads 20 bit imm (<< 12) into upper 20 bits of reg)
+            // rd = imm
+            if(instr.u.rd != ZERO){
+                regs[instr.u.rd] = instr.u.imm;
+            }
+            break;
+        case AUIPC: // Add Upper Immediate to PC (adds 20 bit imm (<< 12) to PC)
+            if(instr.u.rd != ZERO){
+                // rd = PC + imm
+                regs[instr.u.rd] = PC + instr.u.imm;
+            }
+            break;
+        default:
+            return -1; // Invalid U-Type opcode
+    }
+
     return 0;
 }
 int handleBType(decoded_fields instr) {
+    uint32_t rs1 = regs[instr.b.rs1]; // Source register
+    uint32_t rs2 = regs[instr.b.rs2]; // Source register
+    imm_t pcOffset = instr.b.imm; //
+
+    int shouldBranch = 0;
+
+    switch(instr.b.funct3){
+        case F3_000: // BEQ
+            // Branch if equal
+            shouldBranch = (rs1 == rs2);
+            break;
+        case F3_001: // BNE
+            // Branch if not equal
+            shouldBranch = (rs1 != rs2);
+            break;
+        case F3_100: // BLT
+            // Branch if less than (signed)
+            shouldBranch = ((int32_t)(rs1) < (int32_t)(rs2));
+            break;
+        case F3_101: // BGE
+            // Branch if greater than or equal (signed)
+            shouldBranch = ((int32_t)(rs1) >= (int32_t)(rs2));
+            break;
+        case F3_110: // BLTU
+            // Branch if less than (unsigned)
+            shouldBranch = (rs1 < rs2);
+            break;
+        case F3_111: // BGEU
+            // Branch if greater tahn or equal (unsigned)
+            shouldBranch = (rs1 > rs2);
+            break;
+        default:
+            return -1; // Invalid B-Type funct3
+    }
+    
+    if(shouldBranch == 1){
+        PC += pcOffset;
+    }else{
+        PC += 4; // DONT ALSO INCREMENT PC IN MAIN
+    }
+
     return 0;
 }
 int handleJType(decoded_fields instr) {
